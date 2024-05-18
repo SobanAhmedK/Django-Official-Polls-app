524032654a5652572e5b27eda8cbfcfbc1efe9c6
Soban <ahmedsoban648@gmail.com>
2024-05-18 17:51:42 +0500

    Add create Poll -contact - about

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/" , views.DetailView.as_view() , name="details"),
     path("<int:pk>/result" , views.ResultView.as_view() , name="result"),
-    path("<int:question_id>/vote" , views.vote , name="vote")
+    path("<int:question_id>/vote" , views.vote , name="vote"),
+    path("contact/", views.contact , name="contact"),
+    path("about/", views.about , name="about"),
+    path('create/', views.CreateView.as_view(), name='create')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import question,choice
 from django.http import HttpResponse,HttpRequest,Http404,HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
-from django.views import generic
+from django.views import generic,View
 from django.utils import timezone
 
 class IndexView(generic.ListView):
@@ -47,4 +47,48 @@ def vote(request:HttpRequest, question_id:int):
         choice_selected.votes+=1
         choice_selected.save()
     return HttpResponseRedirect(reverse("polls:result", args=(Question.id,)))
+
+
+def contact(request:HttpRequest):
+    return render(request , "polls/contact.html")
+
+
+
+def about(request:HttpRequest):
+    return render(request , "polls/about.html")
+
+
+def contact(request:HttpRequest):
+    return render(request , "polls/contact.html")
+
+class CreateView(View):
+    def get(self, request: HttpRequest):
+        return render(request, 'polls/create.html')
+
+    def post(self, request: HttpRequest):
+        question_text = request.POST.get('question_text')
+        choice_texts = request.POST.getlist('choices')
+        
+        if not question_text or not choice_texts or all(not choice for choice in choice_texts):
+            return render(request, 'polls/create.html', {
+                'error_message': 'You must enter a question and at least one choice.',
+            })
+        
+        # Create the new question
+        Question = question.objects.create(
+            question_text=question_text,
+            publication_date=timezone.now()
+        )
+        
+        # Create the choices related to this question
+        for choice_text in choice_texts:
+            if choice_text.strip():  
+                choice.objects.create(
+                    question=Question,
+                    text_choice=choice_text.strip()
+                )
+        
+        return redirect('polls:index')
+
+
 
